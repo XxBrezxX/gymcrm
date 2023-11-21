fae227da73b408dc2b68980c0021e9f873efdb1b
Bryan Hernandez <bryan_hernandez@epam.com>
2023-11-21 16:36:13 -0600

    Repositories created succesfully, Entities are working properly

--- src/main/java/com/example/gymcrm/repositories/TrainerDao.java
+++ src/main/java/com/example/gymcrm/repositories/TrainerDao.java
@@ -3,8 +3,9 @@ package com.example.gymcrm.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.gymcrm.model.User;
+import com.example.gymcrm.model.Trainer;
 
 @Repository
-public interface UserDao extends JpaRepository<User, Long> {
+public interface TrainerDao extends JpaRepository<Trainer, Long> {
+
 }
--- src/main/java/com/example/gymcrm/repositories/TrainingDao.java
+++ src/main/java/com/example/gymcrm/repositories/TrainingDao.java
@@ -3,8 +3,9 @@ package com.example.gymcrm.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.gymcrm.model.User;
+import com.example.gymcrm.model.Training;
 
 @Repository
-public interface UserDao extends JpaRepository<User, Long> {
+public interface TrainingDao extends JpaRepository<Training, Long> {
+
 }
