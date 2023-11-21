794657873e62a10e5c89b5e84b6f9901b49410ec
Bryan Hernandez <bryan_hernandez@epam.com>
2023-11-21 12:01:41 -0600

    Training Type added as an entity

---
+++ src/main/java/com/example/gymcrm/model/TrainingType.java
@@ -0,0 +1,23 @@
+package com.example.gymcrm.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrainingType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String trainingTypeName;
+    
+}
