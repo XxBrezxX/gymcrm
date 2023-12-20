cb0f2641005d6cd7e8825f601b6a9511dd9d7442
Bryan Hernandez <bryan_hernandez@epam.com>
2023-12-20 11:59:26 -0600

    Test Training Fixed

--- src/test/java/com/example/gymcrm/services/TrainingServiceImplTest.java
+++ src/test/java/com/example/gymcrm/services/TrainingServiceImplTest.java
@@ -21,6 +21,7 @@ import com.example.gymcrm.model.User;
 import com.example.gymcrm.repositories.TrainingDao;
 import com.example.gymcrm.services.implementations.models.TrainingServiceImpl;
 import com.example.gymcrm.services.implementations.web.TrainerWorkloadService;
+import com.example.gymcrm.services.messaging.MessageSenderService;
 
 @DataJpaTest
 class TrainingServiceImplTest {
@@ -29,6 +30,9 @@ class TrainingServiceImplTest {
 
     @Mock
     private TrainingDao trainingDao;
+
+    @Mock
+    private MessageSenderService messageSenderService;
 
     @InjectMocks
     private TrainingServiceImpl trainingServiceImpl;
